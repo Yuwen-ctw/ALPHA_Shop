3f4af76a0e594dce7dbdd9ac8617fce74549d442
Yuwen <kfk07027@gmail.com>
2022-11-03 10:30:12 +0800

    Feat: add React component - ProgressControl

--- src/components/Register/ProgressControl.js
+++ src/components/Register/ProgressControl.js
@@ -1,5 +1,10 @@
 function ProgressControl() {
-
+  return (
+    <div>
+      <button type="button">上一步</button>
+      <button type="button">下一步</button>
+    </div>
+  )
 }
 
 export default ProgressControl
